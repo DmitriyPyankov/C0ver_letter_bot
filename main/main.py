8111bae7b24d328dd0866fb3bb25ca820fb5fada
DmitriyPyankov <pyandoom@mail.ru>
2023-08-18 13:11:57 +0300

    add debage telegram api code

--- main/main.py
+++ main/main.py
@@ -1,35 +1,83 @@
-import requests
-import json
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+import openai
+from config import TOKEN_TG_BOT, OPENAI_API_KEY
+from rezume_parsing import get_rezume
+from vacancy_parsing import get_vacancy
 
-# Замените следующие значения на ваши реальные данные
-client_id = "ваш_client_id"
-redirect_uri = "ваш_redirect_uri"
-state = 121
 
-# Формирование URL для перенаправления пользователя
-authorize_url = f"https://hh.ru/oauth/authorize?response_type=code&client_id={client_id}&state={state}&redirect_uri={redirect_uri}"
+logging.basicConfig(
+    format="%(levelname)s: %(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    level=logging.INFO,
+)
 
-# Открываем URL в браузере и пользователь авторизуется
+bot = Bot(TOKEN_TG_BOT)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-# Замените этот код на код, который будет использован в вашем приложении для получения кода авторизации
-authorization_code = "ваш_код_авторизации"
+openai.api_key = OPENAI_API_KEY
 
-# Замените настоящие данные авторизации
-client_secret = "ваш_client_secret"
 
-# Запрос на получение токена
-token_url = "https://hh.ru/oauth/token"
-data = {
-    "grant_type": "authorization_code",
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "code": authorization_code,
-    "redirect_uri": redirect_uri,
-}
+def get_completion(prompt, model="gpt-3.5-turbo-16k"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0,
+    )
+    return response.choices[0].message["content"]
 
-response = requests.post(token_url, data=data)
-token_data = response.json()
 
-# В этой переменной будет содержаться ваш access token
-access_token = token_data.get("access_token")
-print("Access Token:", access_token)
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    await message.answer("Приветики. Я готов написать для тебя уникальное сопроводительное письмо для любой вакансии. \nВведите ссылку на ваше резюме. Например: https://hh.ru/resume/8734e61f0000f3fcf00039ed1f44455a346c36")
+
+
+@dp.message_handler(regexp="https://hh\.ru/resume/.*")
+async def rezume(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["rezume_url"] = message.text
+    await message.answer("Введите ссылку на интересующую вас вакансию. Например, https://hh.ru/vacancy/84829254")
+
+
+@dp.message_handler(regexp="https://hh\.ru/vacancy/.*")
+async def vacancy(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        rezume_url = data["rezume_url"]
+    vacancy_url = message.text
+    await message.answer("Пишу для вас сопроводительное письмо...")
+
+    rezume = get_rezume(rezume_url)
+    vacancy = get_vacancy(vacancy_url)
+
+    prompt = f"""
+    Напиши сопроводительное письмо на основе описания резюме и вакансии, текст которых выделен в тройные кавычки.
+    ссылка на резюме: '''{rezume}'''
+    сслылка на вакансию: '''{vacancy}'''
+    """
+    response = get_completion(prompt)
+    await message.answer(response)
+    await state.finish()
+
+
+@dp.message_handler()
+async def undefined_message(message: types.Message):
+    await message.answer(
+        "Чтобы начать пользоваться ботом, используйте команду /start",
+        parse_mode="Markdown",
+    )
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
+
+
+# https://hh.ru/resume/8734e61f0000f3fcf00039ed1f44455a346c36
+# https://hh.ru/vacancy/84829254
+
+
+# https://hh.ru/resume/c25565e100075e48200039ed1f4c6269506536?hhtmFrom=resume_search_result
+# https://hh.ru/vacancy/83116089?from=vacancy_search_list&query=python
--- main/rezume_parsing.py
+++ main/rezume_parsing.py
@@ -3,26 +3,34 @@ from bs4 import BeautifulSoup
 import fake_useragent
 import time
 
+
 def get_rezume(text):
     ua = fake_useragent.UserAgent()
-    data = requests.get(
-        url=f'{text}',
-        headers={'user-agent': ua.random}
-    )
+    data = requests.get(url=f"{text}", headers={"user-agent": ua.random})
     if data.status_code != 200:
         return
-    soup = BeautifulSoup(data.content, 'lxml')
+    soup = BeautifulSoup(data.content, "lxml")
 
     try:
-        main = [info.text.replace('\xa0', ' ').replace('\n', '.').replace('\r', '').replace('\t', '').replace('•', '').replace('\u2009', '') for info in soup.find(attrs={'class': 'bloko-gap bloko-gap_top'})]
+        main = [
+            info.text.replace("\xa0", " ")
+            .replace("\n", ".")
+            .replace("\r", "")
+            .replace("\t", "")
+            .replace("•", "")
+            .replace("\u2009", "")
+            for info in soup.find(attrs={"class": "bloko-gap bloko-gap_top"})
+        ]
     except:
-        main = ''
+        main = ""
 
     rezume = [main[i] for i in range(len(main)) if i in [0, 1, 4, 5, 7]]
 
     return rezume
 
-rezume_url = input("Введите URL резюме (например, 'https://hh.ru/resume/8734e61f0000f3fcf00039ed1f44455a346c36'): ")
-REZUME = get_rezume(rezume_url)
 
-
+if __name__ == "__main__":
+    rezume_url = input(
+        "Введите URL резюме (например, 'https://hh.ru/resume/8734e61f0000f3fcf00039ed1f44455a346c36'): "
+    )
+    REZUME = get_rezume(rezume_url)
